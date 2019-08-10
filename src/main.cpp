1750af622acc7788e26f239f58727ecea44395db
Bradford Bazemore <bankersalgo@gmail.com>
2019-08-10 19:21:35 -0400

    works

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@ void setup()
 {
     Serial.begin(115200);
     Serial2.begin(9600);
+
+    pinMode(32, OUTPUT);
+    pinMode(39, INPUT);
+    digitalWrite(32, LOW);
+
 }
 
 void loop()
@@ -11,5 +16,9 @@ void loop()
     while (Serial2.available())
     {
         Serial.print(char(Serial2.read()));
+        if(digitalRead(39)==1){
+            pinMode(32, INPUT);
+            break;
+        }
     }
 }
