1c5ad015fe78ddab1edd071d8e93cd4f2ef4048f
Bradford Bazemore <bankersalgo@gmail.com>
2019-08-28 20:56:31 -0400

    it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,52 @@
 #include <Arduino.h>
 
+void wakeup()
+{
+    uint64_t wakeup_reason = esp_sleep_get_ext1_wakeup_status();
+    uint64_t mask = 1;
+    int i = 0;
+    digitalWrite(12, HIGH);
+    while (i < 64)
+    {
+        if (wakeup_reason & mask){
+            Serial.printf("Wakeup because %d\n", i);
+            digitalWrite(13, HIGH);
+        }
+        i++;
+        mask = mask << 1;
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
-    Serial2.begin(9600);
+    delay(100);
+    Serial.println();
+
+    pinMode(36, INPUT);
+    pinMode(35, INPUT);
+
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+    pinMode(14, OUTPUT);
+    pinMode(15, OUTPUT);
+
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(14, LOW);
+    digitalWrite(15, LOW);
+
+    Serial.printf("system starting...\n");
+    delay(100);
+
+    wakeup();
+    uint64_t wakeMask = 1ULL << 36;
+    esp_sleep_enable_ext1_wakeup(wakeMask, ESP_EXT1_WAKEUP_ANY_HIGH);
+    Serial.println("Going to sleep now");
+    esp_deep_sleep_start();
 }
 
 void loop()
 {
-    while (Serial2.available())
-    {
-        Serial.print(char(Serial2.read()));
-    }
+
 }
