32f9982d327f7987f8cc107e9a1f32f85d4b2ad5
Bradford Bazemore <bankersalgo@gmail.com>
2019-08-29 22:10:59 -0400

    imu works

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 #include "remora-hal.h"
 #include <AutoConnect.h>
 #include "GPS.h"
+#include "SparkFunLSM9DS1.h"
+#include <Wire.h>
 
 #define GPSSerial Serial2
 
 void initGPS();
+void initIMU();
 
 WebServer Server;
 AutoConnect Portal(Server);
 
-TinyGPSPlus gps;
+LSM9DS1 imu;
+
+#define LSM9DS1_M 0x1E  // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG 0x6B // Would be 0x6A if SDO_AG is LOW
+
+#define PRINT_CALCULATED
+#define PRINT_SPEED 250             // 250 ms between prints
+static unsigned long lastPrint = 0; // Keep track of print time
+
+#define DECLINATION -9.13 // Declination (degrees) in Raleigh, NC.
 
 void startPortal()
 {
@@ -26,72 +38,143 @@ void startPortal()
     }
 }
 
-void displayInfo()
+void printGyro()
 {
-    Serial.print(F("Location: "));
-    if (gps.location.isValid())
-    {
-        Serial.print(gps.location.lat(), 6);
-        Serial.print(F(","));
-        Serial.print(gps.location.lng(), 6);
-    }
-    else
-    {
-        Serial.print(F("INVALID"));
-    }
+    // Now we can use the gx, gy, and gz variables as we please.
+    // Either print them as raw ADC values, or calculated in DPS.
+    Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+    // If you want to print calculated values, you can use the
+    // calcGyro helper function to convert a raw ADC value to
+    // DPS. Give the function the value that you want to convert.
+    Serial.print(imu.calcGyro(imu.gx), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcGyro(imu.gy), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcGyro(imu.gz), 2);
+    Serial.println(" deg/s");
+#elif defined PRINT_RAW
+    Serial.print(imu.gx);
+    Serial.print(", ");
+    Serial.print(imu.gy);
+    Serial.print(", ");
+    Serial.println(imu.gz);
+#endif
+}
 
-    Serial.print(F("  Date/Time: "));
-    if (gps.date.isValid())
-    {
-        Serial.print(gps.date.month());
-        Serial.print(F("/"));
-        Serial.print(gps.date.day());
-        Serial.print(F("/"));
-        Serial.print(gps.date.year());
-    }
-    else
-    {
-        Serial.print(F("INVALID"));
-    }
+void printAccel()
+{
+    // Now we can use the ax, ay, and az variables as we please.
+    // Either print them as raw ADC values, or calculated in g's.
+    Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+    // If you want to print calculated values, you can use the
+    // calcAccel helper function to convert a raw ADC value to
+    // g's. Give the function the value that you want to convert.
+    Serial.print(imu.calcAccel(imu.ax), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcAccel(imu.ay), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcAccel(imu.az), 2);
+    Serial.println(" g");
+#elif defined PRINT_RAW
+    Serial.print(imu.ax);
+    Serial.print(", ");
+    Serial.print(imu.ay);
+    Serial.print(", ");
+    Serial.println(imu.az);
+#endif
+}
 
-    Serial.print(F(" "));
-    if (gps.time.isValid())
-    {
-        if (gps.time.hour() < 10)
-            Serial.print(F("0"));
-        Serial.print(gps.time.hour());
-        Serial.print(F(":"));
-        if (gps.time.minute() < 10)
-            Serial.print(F("0"));
-        Serial.print(gps.time.minute());
-        Serial.print(F(":"));
-        if (gps.time.second() < 10)
-            Serial.print(F("0"));
-        Serial.print(gps.time.second());
-        Serial.print(F("."));
-        if (gps.time.centisecond() < 10)
-            Serial.print(F("0"));
-        Serial.print(gps.time.centisecond());
-    }
-    else
-    {
-        Serial.print(F("INVALID"));
-    }
+void printMag()
+{
+    // Now we can use the mx, my, and mz variables as we please.
+    // Either print them as raw ADC values, or calculated in Gauss.
+    Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+    // If you want to print calculated values, you can use the
+    // calcMag helper function to convert a raw ADC value to
+    // Gauss. Give the function the value that you want to convert.
+    Serial.print(imu.calcMag(imu.mx), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcMag(imu.my), 2);
+    Serial.print(", ");
+    Serial.print(imu.calcMag(imu.mz), 2);
+    Serial.println(" gauss");
+#elif defined PRINT_RAW
+    Serial.print(imu.mx);
+    Serial.print(", ");
+    Serial.print(imu.my);
+    Serial.print(", ");
+    Serial.println(imu.mz);
+#endif
+}
 
-    Serial.println();
+// Calculate pitch, roll, and heading.
+// Pitch/roll calculations take from this app note:
+// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
+// Heading calculations taken from this app note:
+// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+    float roll = atan2(ay, az);
+    float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+    float heading;
+    if (my == 0)
+        heading = (mx < 0) ? PI : 0;
+    else
+        heading = atan2(mx, my);
+
+    heading -= DECLINATION * PI / 180;
+
+    if (heading > PI)
+        heading -= (2 * PI);
+    else if (heading < -PI)
+        heading += (2 * PI);
+
+    // Convert everything from radians to degrees:
+    heading *= 180.0 / PI;
+    pitch *= 180.0 / PI;
+    roll *= 180.0 / PI;
+
+    Serial.print("Pitch, Roll: ");
+    Serial.print(pitch, 2);
+    Serial.print(", ");
+    Serial.println(roll, 2);
+    Serial.print("Heading: ");
+    Serial.println(heading, 2);
 }
 
 void dive()
 {
     initGPS();
-    while (GPSSerial.available() > 0)
+    initIMU();
+
+    // Update the sensor values whenever new data is available
+    if (imu.gyroAvailable())
     {
-        if (gps.encode(GPSSerial.read()))
-        {
-            displayInfo();
-        }
-        delay(100);
+        imu.readGyro();
     }
+    if (imu.accelAvailable())
+    {
+        imu.readAccel();
+    }
+    if (imu.magAvailable())
+    {
+        imu.readMag();
+    }
+
+    printGyro();  // Print "G: gx, gy, gz"
+    printAccel(); // Print "A: ax, ay, az"
+    printMag();   // Print "M: mx, my, mz"
+    // Print the heading and orientation for fun!
+    // Call print attitude. The LSM9DS1's mag x and y
+    // axes are opposite to the accelerometer, so my, mx are
+    // substituted for each other.
+    printAttitude(imu.ax, imu.ay, imu.az,
+                  -imu.my, -imu.mx, imu.mz);
+    Serial.println();
 }
 
 void wakeup()
@@ -161,6 +244,22 @@ void initGPS()
     {
     }
     delay(100);
+}
+
+void initIMU()
+{
+    imu.settings.device.commInterface = IMU_MODE_I2C;
+    imu.settings.device.mAddress = LSM9DS1_M;
+    imu.settings.device.agAddress = LSM9DS1_AG;
+
+    Wire.begin(I2C_SDA, I2C_SCL); // this should be done in the library
+
+    if (!imu.begin())
+    {
+        Serial.println("Failed to communicate with LSM9DS1.");
+        Serial.println("Double-check wiring.");
+        while (true);// this is a bad idea
+    }
 }
 
 void setup()
