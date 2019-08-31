ac7e65824948748293a44d4e9cffca16a57d3474
Bradford Bazemore <bankersalgo@gmail.com>
2019-08-30 22:35:30 -0400

    got some time working

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <Arduino.h>
 
+void TaskA(void *parms);
+void TaskB(void *parms);
+
 void setup()
 {
     Serial.begin(115200);
-    Serial2.begin(9600);
+
+    xTaskCreate(TaskA, "TaskA", 10000, NULL, 1, NULL);
+    xTaskCreate(TaskB, "TaskB", 10000, NULL, 2, NULL);
 }
 
-void loop()
+void loop() {}
+
+void TaskA(void *parms)
 {
-    while (Serial2.available())
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    while (true)
     {
-        Serial.print(char(Serial2.read()));
+        Serial.print("Hello from A ");
+        Serial.println(millis()/1000);
+        vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void TaskB(void *parms)
+{
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    while (true)
+    {
+        Serial.print("Hello from B ");
+        Serial.println(millis()/1000);
+        vTaskDelayUntil(&xLastWakeTime, 2000 / portTICK_PERIOD_MS);
     }
 }
