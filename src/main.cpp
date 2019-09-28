bf577519c1f33f1bb55f2fd178513bf3b588ef71
Bradford Bazemore <bankersalgo@gmail.com>
2019-09-28 19:33:37 -0400

    now it will not start the portal

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,15 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <TimeLib.h>
 
-#include <Dive.hpp>
-#include <hal/TSYS01.hpp>
-#include <hal/MS5837.hpp>
-#include <hal/remora-hal.h>
-#include <Storage/SecureDigital.hpp>
-#include <Navigation/GNSS.hpp>
-#include <Utils.hpp>
-
-GNSS gps;
+#include <Wake.hpp>
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(GPIO_SENSOR_POWER, OUTPUT);
-    digitalWrite(GPIO_SENSOR_POWER, LOW);
-    delay(10);
-    Wire.begin(I2C_SDA, I2C_SCL);
-    delay(10);
+    delay(1000);
+    Serial.println("I did an update!");
 
-    Serial.printf("Remora ID:%s", remoraID());
-
-    gps = GNSS();
-    SecureDigital sd = SecureDigital();
-    Dive d(&sd);
-    tsys01 temperatureSensor = tsys01();
-    ms5837 depthSensor = ms5837();
-    if (d.Start(now(), gps.getLat(), gps.getLng()) == "")
-    {
-        Serial.println("error starting the dive");
-    }
-    else
-    {
-        for (int i = 0; i < 300; i++)
-        {
-            Record tempRecord = Record{temperatureSensor.getTemp(), depthSensor.getDepth()};
-            d.NewRecord(tempRecord);
-        }
-        if (d.End(now(), gps.getLat(), gps.getLng()) == "")
-        {
-            Serial.println("error ending the dive");
-        }
-    }
-    Serial.println("done");
+    wake();
+    sleep();
 }
 
 void loop()
